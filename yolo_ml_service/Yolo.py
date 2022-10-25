d76984754df0316616533cf8d27202c78f84cb95
Trevor Royer <troyer@redhat.com>
2022-10-25 17:40:04 -0500

    Update model load process

--- yolo_ml_service/Yolo.py
+++ yolo_ml_service/Yolo.py
@@ -19,10 +19,12 @@ class Yolo:
         logging.info(f"Loading model from {self.model_file}")
 
         try:
-            self.model = torch.load(self.model_file)
+            self.model = torch.load(self.model_file, map_location=torch.device('cpu'))
         except IOError:
             logging.exception(f"Unable to load the modelfile: {self.model_file}")
             sys.exit(errno.ENOENT)
+
+        self.model.eval()
 
     def predict(self, X, features_names):
         logging.debug(f"Performing prediction on feature_names: {features_names} and X: {X}")
